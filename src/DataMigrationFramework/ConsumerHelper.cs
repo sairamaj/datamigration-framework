c50d3bc5015142743f2e8d931c65545f60d3d2ab
Sairama <sairamaj@gmail.com>
2020-01-19 16:04:20 -0800

    Added ConsumerHelper unit tests

--- src/DataMigrationFramework/ConsumerHelper.cs
+++ src/DataMigrationFramework/ConsumerHelper.cs
@@ -5,17 +5,53 @@ using System.Threading.Tasks;
 
 namespace DataMigrationFramework
 {
+    /// <summary>
+    /// Helper class to distribute items to consume.
+    /// </summary>
+    /// <typeparam name="T">
+    /// Type name of the model.
+    /// </typeparam>
     internal class ConsumerHelper<T>
     {
+        /// <summary>
+        /// Destination instance.
+        /// </summary>
         private readonly IDestination<T> _destination;
+
+        /// <summary>
+        /// Number of consumers.
+        /// </summary>
         private readonly int _numberOfConsumers;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ConsumerHelper{T}"/> class.
+        /// </summary>
+        /// <param name="destination">
+        /// A <see cref="IDestination{T}"/> instance.
+        /// </param>
+        /// <param name="numberOfConsumers">
+        /// Number of consumers.
+        /// </param>
         public ConsumerHelper(IDestination<T> destination, int numberOfConsumers)
         {
-            _destination = destination;
-            _numberOfConsumers = numberOfConsumers;
+            if (numberOfConsumers <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(numberOfConsumers), numberOfConsumers, "Number of consumers should be > 0");
+            }
+
+            this._destination = destination ?? throw new ArgumentNullException(nameof(destination));
+            this._numberOfConsumers = numberOfConsumers;
         }
 
+        /// <summary>
+        /// Consume given items.
+        /// </summary>
+        /// <param name="items">
+        /// A <see cref="IEnumerable{T}"/> of items to pass it to destination consumer.
+        /// </param>
+        /// <returns>
+        /// Actual items consumed successfully.
+        /// </returns>
         public int Consume(IEnumerable<T> items)
         {
             items = items.ToList();
@@ -32,10 +68,16 @@ namespace DataMigrationFramework
             }
 
             var perConsumerSize = size / actualConsumers;
+            var lastSize = size % actualConsumers;
             var skip = 0;
             var tasks = new List<Task<int>>();
             for (var i = 0; i < actualConsumers; i++)
             {
+                if (i == actualConsumers-1)
+                {
+                    perConsumerSize += lastSize;
+                }
+
                 Console.WriteLine($"skip {skip} perConsumerSize:{perConsumerSize}");
                 var consumerItems = items.Skip(skip).Take(perConsumerSize);
                 tasks.Add(this._destination.ConsumeAsync(consumerItems));
---
+++ test/DataMigrationFramework.Unit.Test/ConsumerHelperTest.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NUnit.Framework;
+using Rhino.Mocks;
+
+namespace DataMigrationFramework.Unit.Test
+{
+    [TestFixture]
+    public class ConsumerHelperTest
+    {
+        delegate Task<int> ConsumerDelegate<T>(IEnumerable<T> items);
+
+        [Test(Description = "Ctor with null destination is not allowed.")]
+        public void CtorWithNullDestinationShouldThrowException()
+        {
+            Action ctorWithNullDestination = () => new ConsumerHelper<string>(null, 10);
+            ctorWithNullDestination.Should().Throw<ArgumentNullException>().WithMessage("Value cannot be null.\r\nParameter name: destination");
+        }
+
+        [Test(Description = "Number of consumers cannot be zero.")]
+        public void CtorWithZeroNumberOfConsumersShouldThrowException()
+        {
+            Action ctorWithZeroConsumers = () => new ConsumerHelper<string>(MockRepository.GenerateMock<IDestination<string>>(), 0);
+            ctorWithZeroConsumers.Should().Throw<ArgumentOutOfRangeException>().WithMessage("Number of consumers should be > 0\r\nParameter name: numberOfConsumers\r\nActual value was 0.");
+        }
+
+        [Test(Description = "Number of consumers cannot less than zero.")]
+        public void CtorWithNegativeConsumersShouldThrowException()
+        {
+            Action ctorWithZeroConsumers = () => new ConsumerHelper<string>(MockRepository.GenerateMock<IDestination<string>>(), -1);
+            ctorWithZeroConsumers.Should().Throw<ArgumentOutOfRangeException>().WithMessage("Number of consumers should be > 0\r\nParameter name: numberOfConsumers\r\nActual value was -1.");
+        }
+
+        [Test(Description = "Zero items should not even try to call destination consum.e")]
+        public void ConsumeWithZeroItemsShouldNotCallDestinationConsume()
+        {
+            // Arrange
+            var mockDestination = MockRepository.GenerateMock<IDestination<string>>();
+            var helper = new ConsumerHelper<string>(mockDestination, 1);
+
+            // Act
+            var consumed = helper.Consume(new string[] { });
+
+            consumed.Should().Be(0);
+            mockDestination.AssertWasNotCalled(destination => destination.ConsumeAsync(new string[] { }));
+        }
+
+        [Test(Description = "With one consumer should call destination consume with same number of parameters.")]
+        public void ConsumeWithOneConsumerShouldCallWithExactRecords()
+        {
+            // Arrange
+            var mockDestination = MockRepository.GenerateMock<IDestination<string>>();
+            mockDestination.Stub(destination => destination.ConsumeAsync(new[] { "one", "two", "three" }))
+                .IgnoreArguments()
+                .Do((ConsumerDelegate<string>)(items =>
+                {
+                    Console.WriteLine("--------------");
+                    return Task.FromResult(items.Count());
+                }));
+            var helper = new ConsumerHelper<string>(mockDestination, 1);
+
+            // Act
+            var consumed = helper.Consume(new[] { "one", "two", "three" });
+
+            consumed.Should().Be(3);
+            mockDestination.AssertWasCalled(
+                destination =>
+                    destination.ConsumeAsync(Arg<IEnumerable<string>>.Matches(actual => Compare(actual, new string[]{"one","two","three"}))));
+        }
+
+        [Test(Description = "One Consumers are greater than size, should use less consumers.")]
+        public void ConsumesMoreThanItemsShouldUseLessConsumers()
+        {
+            var rand = new Random();
+            var consumeData = new Dictionary<int, IEnumerable<string>>();
+            // Arrange
+            var mockDestination = MockRepository.GenerateMock<IDestination<string>>();
+            mockDestination.Stub(destination => destination.ConsumeAsync(new[] { "one", "two" }))
+                .IgnoreArguments()
+                .Do((ConsumerDelegate<string>)(items =>
+                {
+                    consumeData[rand.Next()] = items.ToList();
+                    Console.WriteLine("-------------");
+                    foreach (var item in items)
+                    {
+                        Console.WriteLine(item);
+                    }
+                    Console.WriteLine("-------------");
+                    return Task.FromResult(items.Count());
+                }));
+            var helper = new ConsumerHelper<string>(mockDestination, 3);
+
+            // Act
+            var consumed = helper.Consume(new[] { "one", "two"});
+
+            consumed.Should().Be(2);
+            consumeData.Keys.Count.Should().Be(2);      // two consumers should exists
+            // todo: match
+        }
+
+        [Test(Description = "One Consumers are equal to size each consumer should call with one item.")]
+        public void ConsumesEqualToSizeShouldCallDestinationWithOneItemEach()
+        {
+            var rand = new Random();
+            var consumeData = new Dictionary<int, IEnumerable<string>>();
+            // Arrange
+            var mockDestination = MockRepository.GenerateMock<IDestination<string>>();
+            mockDestination.Stub(destination => destination.ConsumeAsync(new[] { "one", "two" , "three"}))
+                .IgnoreArguments()
+                .Do((ConsumerDelegate<string>)(items =>
+                {
+                    consumeData[rand.Next()] = items.ToList();
+                    Console.WriteLine("-------------");
+                    foreach (var item in items)
+                    {
+                        Console.WriteLine(item);
+                    }
+                    Console.WriteLine("-------------");
+                    return Task.FromResult(items.Count());
+                }));
+            var helper = new ConsumerHelper<string>(mockDestination, 3);
+
+            // Act
+            var consumed = helper.Consume(new[] { "one", "two", "three" });
+
+            consumed.Should().Be(3);
+            consumeData.Keys.Count.Should().Be(3);      // two consumers should exists
+            // todo: match
+        }
+
+        [Test(Description = "One Consumers are less than size, should all consumers with each size more than 1.")]
+        public void ConsumesLessThanSizeShouldCallDestination()
+        {
+            var rand = new Random();
+            var consumeData = new Dictionary<int, IEnumerable<string>>();
+            // Arrange
+            var mockDestination = MockRepository.GenerateMock<IDestination<string>>();
+            mockDestination.Stub(destination => destination.ConsumeAsync(new[] { "one", "two", "three","four","five","six","seven","eight","nine","ten" }))
+                .IgnoreArguments()
+                .Do((ConsumerDelegate<string>)(items =>
+                {
+                    consumeData[rand.Next()] = items.ToList();
+                    Console.WriteLine("-------------");
+                    foreach (var item in items)
+                    {
+                        Console.WriteLine(item);
+                    }
+                    Console.WriteLine("-------------");
+                    return Task.FromResult(items.Count());
+                }));
+            var helper = new ConsumerHelper<string>(mockDestination, 3);
+
+            // Act
+            var consumed = helper.Consume(new[] { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten" });
+
+            consumed.Should().Be(10);
+            consumeData.Keys.Count.Should().Be(3);      // two consumers should exists
+            // todo: match
+        }
+
+
+        private bool Compare(IEnumerable<string> actual, IEnumerable<string> expected)
+        {
+            actual.Should().BeEquivalentTo(expected);
+            return true;
+        }
+    }
+}
